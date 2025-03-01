b0d24fef8b921ad03287742ec7d45236286fee79
MalMen <git@pedrogaspar.pt>
2025-03-01 00:32:25 -0100

    [ADD] python check

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source install/python.sh
 source install/hellyes.sh
 source install/device_wvd.sh
 source install/browsers.sh
@@ -7,17 +8,18 @@ source install/ffmpeg.sh
 source install/bento4.sh
 # --- Main Installation ---
 
-# Install Python environment first
-#install_hellyes
+ensure_python_installed
+
+install_hellyes
 
 install_device_wvd
 
-#install_n_m3u8dl_re
+install_n_m3u8dl_re
 
-#install_ffmpeg
+install_ffmpeg
 
-#install_mp4decrypt
+install_mp4decrypt
 
-#install_browsers_manifest
+install_browsers_manifest
 
 echo "✅ Installation complete!"
